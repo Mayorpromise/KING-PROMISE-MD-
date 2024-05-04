143c776a5d3ec81470cac0f96632b6dcf24fd683
Excel Amadi <ms.excel.amadi@gmail.com>
2024-05-04 03:11:11 +0100

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -20,7 +20,7 @@ let handler = async (m, { conn, usedPrefix, command}) => {
     let uptime = clockString(_uptime)
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-let pp = './Assets/STAR-V2.jpg'
+let pp = './STAR.jpg'
 let user = global.db.data.users[who]
 let { name, exp, diamond, lastclaim, registered, regTime, age, level, role, warn } = global.db.data.users[who]
 let { min, xp, max } = xpRange(user.level, global.multiplier)
@@ -37,50 +37,524 @@ let quote = quotes[Math.floor(Math.random() * quotes.length)];
 
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
 let str = `
-╭────≪丂ㄒ卂尺≫───────╮
-╽ 🤖 _BOT STATUS:_🤖
-┃ 🏮  *Founder:* 乇乂匚乇ㄥ
-┃ 🤡  *Bot Name:*©𝚂𝚃𝙰𝚁-𝙼𝙳-𝚅𝟸
-┃ 💻  *Host:* 𝙻𝙸𝙽𝚄𝚇 𝙻𝙰𝙸𝚁
+
+┏━━🤖 _BOT STATUS:_🤖━━┓
+┃ 🏮  *Creator:* Excel Amadi
+┃ 🤡  *Bot Name:* STAR-MD-V2
+┃ 💻  *Host:* Linux Lair
 ┃ 📣  *Prefix:* ${usedPrefix} 
 ┃ 🕓  *Uptime:* ${uptime}
 ┃ 💌  *Database:* ${rtotalreg} of ${totaluser} 
 ┃ 📚  *Total Users:* ${totaluser} 
-╰───────────────⍟
+╰───────────────❖
 
 ┏━━⏰ _Today's Sauce!_ ⏰━┓
 ┃ 📆  *Today's Date:* ${date} 
 ┃ ⏲️  *Current Time:* ${wib} 
-╰───────────────⍟
+╰───────────────❖
 
-┏━👤 `User 𝙸𝚗𝚏𝚘`👤━┓
+┏━*👤USER*━┓
 ┃ 👾  *User Tag:* ${taguser} 
 ┃ 😇  *Name:* ${name} 
-┃ 🌟  *Master Mind:* 乇乂匚乇ㄥ
-┃ 💎  *Diamonds:* ${diamond} 
+┃ 🌟  *Master Mind:* Excel
+┃ 💎  *Diamonds:* -9999
 ┃ 🏆  *Rank:* ${role}
 ┃ 🎮  *XP:* ${exp} 
-╰───────────────⍟
-┌─ℹ️『ᏆΝͲᎬᏞ-ᏟϴᎡᎬ  』ℹ️
-┃📡Cores: 8
-┃🖇Threads: 16
-┃🕑Base Clock Speed: 3.8 GHz
-┃♻️Max Turbo Boost Speed:5.1 ┃GHz
-┃🗜Cache: 16 MB Intel Smart ┃Cache
-┃📲Lithography: 14 nm
-┃💻TDP (Thermal Design ┃Power): 125 W
-┃🖲Socket: LGA 1200
-┃💽Memory Support: ┃DDR4-2933
-┃📀Integrated Graphics:*l Intel ┃UHD Graphics 630
-┃⚙️Total Commands:-*_%totalfeatures_*
+╰───────────────❖
+
+ 
+┌─❖『 *_religion_* 』❖
+┃❏*bible [chapter_number|chapter_name]
+❏*quran [surah_number|surah_name]
 ╰─────────────────❖
-`
-   conn.sendFile(m.chat, pp, 'perfil.jpg', str, m, null, rpyt)
+┌─❖『 *_AI_* 』❖
+┃❏*bingimg2
+┃❏*chatgpt
+┃❏*aisearch
+┃❏*toanime
+┃❏*gitagpt
+┃❏*chat
+┃❏*blackbox
+┃❏*bingimg <query>
+┃❏*tocartoon
+┃❏*dalle
+╰─────────────────❖
+┌─❖『 *_image_* 』❖
+┃❏*blackpink
+╰─────────────────❖
+┌─❖『 *_tools_* 』❖
+┃❏*gemini <text>
+┃❏*nowa
+┃❏*qr <text>
+┃❏*qrcode <text>
+┃❏*weather *<place>*
+┃❏*shortlink <longLink>
+┃❏*bitly <longlink>
+┃❏*dehaze
+┃❏*recolor
+┃❏*hdr
+┃❏*get
+┃❏*length <amount>
+┃❏*tinyurl <link>
+┃❏*shorten <link>
+┃❏*tempmail
+┃❏*shazam
+┃❏*cal <equation>
+┃❏*.carbon <code>
+┃❏*define <word>
+┃❏*element
+┃❏*itunes
+┃❏*lyrics
+┃❏*imdb
+┃❏*course
+┃❏*randomcourse
+┃❏*readmore <text1>|<text2>
+┃❏*readvo
+┃❏*removebg
+┃❏*ss <url>
+┃❏*ssf <url>
+┃❏*style <key> <text>
+┃❏*subreddit
+┃❏*telesticker  Ⓛ
+┃❏*tourl
+┃❏*translate <lang> <text>
+┃❏*tts <lang> <task>
+┃❏*wa
+┃❏*wikipedia
+┃❏*true
+┃❏*findmusic
+┃❏*githubstalk <query>
+╰─────────────────❖
+┌─❖『 *_group_* 』❖
+┃❏*getbio <@tag/reply>  Ⓛ
+┃❏*getbio <@tag/reply>  Ⓛ
+┃❏*animequote
+┃❏*Setdesc <text>
+┃❏*setname <text>
+┃❏*add
+┃❏*delete
+┃❏*delwarn @user
+┃❏*demote (@tag)
+┃❏*infogp
+┃❏*hidetag
+┃❏*invite <917xxx>
+┃❏*kick @user
+┃❏*link
+┃❏*poll question|option|option
+┃❏*profile
+┃❏*promote
+┃❏*resetlink
+┃❏*setbye <text>
+┃❏*group *open/close*
+┃❏*setwelcome <text>
+┃❏*simulate <event> @user
+┃❏*staff
+┃❏*tagall
+┃❏*totag
+┃❏*warn @user
+┃❏*warns
+╰─────────────────❖
+┌─❖『 *_anime_* 』❖
+┃❏*anime
+┃❏*akira
+┃❏*akiyama
+┃❏*anna
+┃❏*asuna
+┃❏*ayuzawa
+┃❏*boruto
+┃❏*chiho
+┃❏*chitoge
+┃❏*deidara
+┃❏*erza
+┃❏*elaina
+┃❏*eba
+┃❏*emilia
+┃❏*hestia
+┃❏*hinata
+┃❏*inori
+┃❏*isuzu
+┃❏*itachi
+┃❏*itori
+┃❏*kaga
+┃❏*kagura
+┃❏*kaori
+┃❏*keneki
+┃❏*kotori
+┃❏*kurumi
+┃❏*madara
+┃❏*mikasa
+┃❏*miku
+┃❏*minato
+┃❏*naruto
+┃❏*nezuko
+┃❏*sagiri
+┃❏*sasuke
+┃❏*sakura
+┃❏*akira
+┃❏*amv  Ⓛ
+┃❏*waifu
+┃❏*neko
+┃❏*zerotwo
+┃❏*loli
+┃❏*jjanime  Ⓛ
+┃❏*pokedex <pokemon>
+┃❏*trace
+╰─────────────────❖
+┌─❖『 *_img_* 』❖
+┃❏*messi
+┃❏*cristianoronaldo
+┃❏*cr7
+┃❏*ppcouple
+┃❏*ppcp
+┃❏*pinterest
+╰─────────────────❖
+┌─❖『 *_owner_* 』❖
+┃❏*leavegc
+┃❏*out
+┃❏*deletechat
+┃❏*pin
+┃❏*unpin
+┃❏*deletechat
+┃❏*addprem <@tag>
+┃❏*addowner @user
+┃❏*allow <@tag>
+┃❏*HEROKU
+┃❏*ban @user
+┃❏*banchat
+┃❏*tx
+┃❏*broadcastgroup <text>
+┃❏*bcgc <text>
+┃❏*cleartmp
+┃❏*delexpired
+┃❏*delprem @user
+┃❏*removeowner @user
+┃❏*setppbotfull
+┃❏*getplugin <name file>
+┃❏*getfile <name file>
+┃❏*join <chat.whatsapp.com> <dias>
+┃❏*reset <54xxx>
+┃❏*resetprefix
+┃❏*restart
+┃❏*.setprefix
+┃❏*.setprefix [symbol]
+┃❏*unban @user
+┃❏*unbanchat
+┃❏*update
+┃❏*update now
+┃❏*config
+┃❏*listban
+┃❏*deleteplugin <name>
+╰─────────────────❖
+┌─❖『 *_fun_* 』❖
+┃❏*afk <reason>
+┃❏*tomp3
+┃❏*toav
+┃❏*toxicity
+┃❏*bot
+┃❏*character @tag
+┃❏*dare
+┃❏*flirt
+┃❏*gay @user
+┃❏*joke
+┃❏*pickupline
+┃❏*question
+┃❏*shayari
+┃❏*ship
+┃❏*yomamajoke
+┃❏*truth
+┃❏*waste @user
+┃❏*perv
+┃❏*handsome
+┃❏*hot
+┃❏*sexy
+┃❏*ugly
+┃❏*cute
+┃❏*playboy
+┃❏*playgirl
+┃❏*beautiful
+┃❏*lesbian
+┃❏*whore
+┃❏*motherfucker
+┃❏*sucker
+┃❏*horny
+┃❏*foolish
+┃❏*nibba
+┃❏*nibbi
+┃❏*bitch
+┃❏*waifu
+┃❏*crackhead
+┃❏*rascal
+┃❏*idiot
+┃❏*girlyboy
+┃❏*tomboy
+┃❏*gigachad
+┃❏*mf
+┃❏*introvert
+┃❏*extrovert
+┃❏*sigma
+┃❏*psycho
+┃❏*brainless
+┃❏*useless
+┃❏*singer
+┃❏*image
+┃❏*meme
+┃❏*quote
+╰─────────────────❖
+┌─❖『 *_reaction_* 』❖
+┃❏*bully @tag
+┃❏*cuddle @tag
+┃❏*cry @tag
+┃❏*hug @tag
+┃❏*awoo @tag
+┃❏*kiss @tag
+┃❏*lick @tag
+┃❏*pat @tag
+┃❏*smug @tag
+┃❏*bonk @tag
+┃❏*yeet @tag
+┃❏*blush @tag
+┃❏*smile @tag
+┃❏*wave @tag
+┃❏*highfive @tag
+┃❏*handhold @tag
+┃❏*nom @tag
+┃❏*bite @tag
+┃❏*glomp @tag
+┃❏*slap @tag
+┃❏*kill @tag
+┃❏*happy @tag
+┃❏*wink @tag
+┃❏*poke @tag
+┃❏*dance @tag
+┃❏*cringe @tag
+╰─────────────────❖
+┌─❖『 *_utility_* 』❖
+┃❏*bmi
+┃❏*crypto
+┃❏*currency
+┃❏*countdown
+┃❏*prayertime
+┃❏*convert
+╰─────────────────❖
+┌─❖『 *_main_* 』❖
+┃❏*channel
+┃❏*database
+┃❏*user
+┃❏*gpguru
+┃❏*alive
+┃❏*blocklist
+┃❏*info
+┃❏*owner
+┃❏*totalfeature
+┃❏*hack
+┃❏*list
+┃❏*botmenu
+┃❏*ownermenu
+┃❏*groupmenu
+┃❏*dlmenu
+┃❏*downloadermenu
+┃❏*economymenu
+┃❏*funmenu
+┃❏*gamemenu
+┃❏*stickermenu
+┃❏*nsfwmenu
+┃❏*logomenu
+┃❏*toolmenu
+┃❏*animemenu2
+┃❏*animemenu
+┃❏*listprem
+┃❏*logomenu2
+┃❏*ping
+┃❏*ping2
+┃❏*runtime
+┃❏*script
+┃❏*server
+┃❏*system
+┃❏*blocklist
+┃❏*setprivacy
+┃❏*allmenu
+╰─────────────────❖
+┌─❖『 *_cmd_* 』❖
+┃❏*delcmd <text>
+┃❏*listcmd
+┃❏*setcmd <txt>
+╰─────────────────❖
+┌─❖『 *_information_* 』❖
+┃❏*cninfo
+┃❏*fact
+╰─────────────────❖
+┌─❖『 *_finance_* 』❖
+┃❏*crypto
+╰─────────────────❖
+┌─❖『 *_cryptocurrency_* 』❖
+┃❏*crypto
+╰─────────────────❖
+┌─❖『 *_time_* 』❖
+┃❏*countdown
+╰─────────────────❖
+┌─❖『 *_downloader_* 』❖
+┃❏*facebook <url>
+┃❏*gdrive 🅟
+┃❏*gitclone <url>
+┃❏*igstalk
+┃❏*instagram
+┃❏*mediafire <url>
+┃❏*mega
+┃❏*modapk
+┃❏*play <query>
+┃❏*play2 <text>
+┃❏*playvid <text>
+┃❏*play3  Ⓛ
+┃❏*tiktok <url>
+┃❏*tiktokstalk
+┃❏*twitter <url>
+┃❏*ytmp3 <url>
+┃❏*ytsearch
+┃❏*ytmp4 <yt-link>
+┃❏*wallpaper <query>
+┃❏*play  Ⓛ
+┃❏*play  Ⓛ
+╰─────────────────❖
+┌─❖『 *_premium_* 』❖
+┃❏*gdrive 🅟
+┃❏*mediafire <url>
+╰─────────────────❖
+┌─❖『 *_economy_* 』❖
+┃❏*addgold <@user>
+┃❏*addxp <@user>
+┃❏*bank
+┃❏*buych
+┃❏*cock-fight <amount>
+┃❏*buy
+┃❏*buyall
+┃❏*daily
+┃❏*deposit
+┃❏*gamble <amount> <color(red/black)>
+┃❏*give credit [amount] [@tag]
+┃❏*levelup
+┃❏*rank
+┃❏*rob
+┃❏*roulette <amount> <color(red/black)>
+┃❏*wallet
+┃❏*withdraw
+┃❏*work
+╰─────────────────❖
+┌─❖『 *_core_* 』❖
+┃❏*leaderboard
+╰─────────────────❖
+┌─❖『 *_game_* 』❖
+┃❏*slot <amount>
+┃❏*chess [from to]
+┃❏*chess delete
+┃❏*chess join
+┃❏*chess start
+┃❏*delttt
+┃❏*guessflag
+┃❏*Maths <modes>
+┃❏*ppt <rock/paper/scissors>
+┃❏*tictactoe <tag number>
+╰─────────────────❖
+┌─❖『 *_config_* 』❖
+┃❏*enable <option>
+┃❏*disable <option>
+╰─────────────────❖
+┌─❖『 *_internet_* 』❖
+┃❏*google <search>
+┃❏*s <search>
+╰─────────────────❖
+┌─❖『 *_maker_* 』❖
+┃❏*blur
+┃❏*difuminar2
+┃❏*hornycard
+┃❏*hornylicense
+┃❏*gfx1
+┃❏*gfx2
+┃❏*gfx3
+┃❏*gfx4
+┃❏*gfx5
+┃❏*gfx6
+┃❏*gfx7
+┃❏*gfx8
+┃❏*gfx9
+┃❏*gfx10
+┃❏*gfx11
+┃❏*gfx12
+┃❏*simpcard
+┃❏*itssostupid
+┃❏*iss
+┃❏*stupid
+┃❏*tweet <comment>
+┃❏*lolicon
+┃❏*quozio
+┃❏*qmkr
+┃❏*ytcomment <comment>
+╰─────────────────❖
+┌─❖『 *_nsfw_* 』❖
+┃❏*nsfw
+╰─────────────────❖
+┌─❖『 *_advanced_* 』❖
+┃❏$
+╰─────────────────❖
+┌─❖『 *_plugin_* 』❖
+┃❏*plugins
+┃❏*install <Gist URL>
+╰─────────────────❖
+┌─❖『 *_rg_* 』❖
+┃❏*reg <name.age>
+┃❏*mysn
+┃❏*unreg <Num Serie>
+╰─────────────────❖
+┌─❖『 *_sticker_* 』❖
+┃❏*emojimix <emoji+emoji>
+┃❏*getsticker
+┃❏*smaker
+┃❏*stickerwithmeme (caption|reply media)
+┃❏*swmeme <url>
+┃❏*swm(caption|reply media)
+┃❏*sfull
+┃❏*toimg <sticker>
+┃❏*tovid
+┃❏*trigger <@user>
+┃❏*ttp
+┃❏*ttp2
+┃❏*ttp3
+┃❏*ttp4
+┃❏*ttp5
+┃❏*attp
+┃❏*attp2
+┃❏*attp3
+┃❏*take <name>|<author>
+╰─────────────────❖
+┌─❖『 *_audio_* 』❖
+┃❏*bass [vn]
+┃❏*blown [vn]
+┃❏*deep [vn]
+┃❏*earrape [vn]
+┃❏*fast [vn]
+┃❏*fat [vn]
+┃❏*nightcore [vn]
+┃❏*reverse [vn]
+┃❏*robot [vn]
+┃❏*slow [vn]
+┃❏*smooth [vn]
+┃❏*tupai [vn]
+╰─────────────────❖
+┌─❖『 *_news_* 』❖
+┃❏*news
+┃❏*technews
+╰─────────────────❖
+┌─❖『 *_conversion_* 』❖
+┃❏*convert
+╰─────────────────❖
+©𝚂𝚃𝙰𝚁-𝙼𝙳-𝚅𝟸 𝙱𝚈 𝙴𝚇𝙲𝙴𝙻 𝙰𝙼𝙰𝙳𝙸`
+
+
+    conn.sendFile(m.chat, pp, 'perfil.jpg', str, m, null, rpyt)
     m.react(done)
 
 }
 handler.help = ['main']
-handler.tags = ['main']
+handler.tags = ['group']
 handler.command = ['menu', 'help'] 
 
 export default handler
