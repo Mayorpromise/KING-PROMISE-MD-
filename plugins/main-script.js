d5ea6b0f013e5159529e88c7a51dc1380ca27b2f
Excel Amadi <ms.excel.amadi@gmail.com>
2024-05-20 00:10:07 +0100

    Update main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -21,8 +21,7 @@ let handler = async function (m, { conn, __dirname }) {
 📝 *ᎠᎬՏᏟᎡᏆᏢͲᏆϴΝ:* _Ꭺ ᏔᎻᎪͲՏᎪᏢᏢ ᏴϴͲ ᎡᎬᏟϴᎡᎠᎬᎠ ᏴᎽ ᎬХᏟᎬᏞ... ᏔᏆͲᎻ ᏞϴͲՏ ϴҒ ᎡᏆᏟᎻ ҒႮΝᏟͲᏆϴΝՏ.... 
 👤 *ᎠᎬᏙᎬᏞϴᏢᎬᎡ:* _EXCEL AMADI_
 ⭐ *ՏͲᎪᎡՏ:* _${repoData.stargazers_count}_ 
-🍴 *ҒϴᎡᏦՏ:* _${repoData.forks_count}_   
-🔍 *ᎦᏟᎪΝ-ϘᎡ:*https://session.guruapi.tech/
+🍴 *ҒϴᎡᏦՏ:* _${repoData.forks_count}_ 
 ⚔️ *ᘜᖇOᑌᑭ:* https://chat.whatsapp.com/Lg0lY4M1k8oDMYzylg86xs
 📡 *ᑭᑌᗷᒪIᑕ ᏀᖇOᑌᑭ:* https://chat.whatsapp.com/EmP3syvou18HrZk6R6nTAK
 💻 *ᑕᕼᗩᑎᑎᗴᒪ ᒪIᑎK:* https://whatsapp.com/channel/0029Va9wmuz8F2pGIURwmo0m 
