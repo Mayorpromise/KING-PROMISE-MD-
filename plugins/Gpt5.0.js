7f821c83c6a127b8bfa20f96628a0d2e569574d8
Excel Amadi <ms.excel.amadi@gmail.com>
2024-05-19 23:28:40 +0100

    Update Gpt5.0.js

--- plugins/Gpt5.0.js
+++ plugins/Gpt5.0.js
@@ -1,36 +1,51 @@
-import fetch from 'node-fetch';
+import displayLoadingScreen from '../lib/loading.js'
+import fetch from 'node-fetch'
+import { delay } from '@whiskeysockets/baileys'
 
-const handler = async (m, { conn, text, usedPrefix, command }) => {
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
   try {
-    if (!text && !(m.quoted && m.quoted.text)) {
-      throw `*_Need query._*\n*_Example:_* _${usedPrefix + command} When did Jesus Christ die?_`;
-    }
+    if (!text) throw `uhm.. what do you want to say?`
+    m.react('🤖')
+    //await displayLoadingScreen(conn, m.chat)
 
-    if (!text && m.quoted && m.quoted.text) {
-      text = m.quoted.text;
-    }
+    const prompt = encodeURIComponent(text)
+    let apiurl = `https://ultimetron.guruapi.tech/gpt4?prompt=${prompt}`
 
-    await m.react('⚡');
-
-    const API_URL = `https://api.maher-zubair.tech/ai/youai?q=${encodeURIComponent(text)}`;
-
-    const response = await fetch(API_URL);
-    const data = await response.json();
-
-    await m.react('✅');
-
-    if (data.status && data.data) {
-      const respuestaAPI = data.data;
-      conn.reply(m.chat, respuestaAPI, m);
-    } else {
-      throw '*Could not get a valid answer, sorry 😅.*';
-    }
+    const result = await fetch(apiurl)
+    const response = await result.json()
+    console.log(response)
+    const textt = response.result.reply
+    await typewriterEffect(conn, m, m.chat, textt)
   } catch (error) {
-    throw `*Oops, an API error occurred. Please try again later 😅.*`;
+    console.error(error)
+    m.reply('Oops! Something went wrong. , we are trying had to fix it asap')
   }
-};
+}
+handler.help = ['gemini <text>']
+handler.tags = ['tools']
+handler.command = /^(gpt4)$/i
 
-handler.command = /^gpt5$/i;
-handler.tags = ['study'];
+export default handler
 
-export default handler;
+async function typewriterEffect(conn, quoted, from, text) {
+  let { key } = await conn.sendMessage(from, { text: 'Thinking...' }, { quoted: quoted })
+
+  for (let i = 0; i < text.length; i++) {
+    const noobText = text.slice(0, i + 1)
+    await conn.relayMessage(
+      from,
+      {
+        protocolMessage: {
+          key: key,
+          type: 14,
+          editedMessage: {
+            conversation: noobText,
+          },
+        },
+      },
+      {}
+    )
+
+    await delay(100) // Adjust the delay time (in milliseconds) as needed
+  }
+}
