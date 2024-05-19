4bb68479657e34021e4153e3cb8a2032373a08bd
Excel Amadi <ms.excel.amadi@gmail.com>
2024-05-19 23:19:56 +0100

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -5,11 +5,11 @@ let name = conn.getName(m.sender)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
 let av = `./Assets/mp3/${pickRandom(["STAR", "STAR1", "STAR2", "STAR3", "STAR4"])}.mp3`
 
-m.reply( `Hello ${taguser} Am STAR-MD-V2 created by Excel Need help?  type /help `)
+m.reply( `Hello ${taguser} Am STAR-MD-V2 created by Excel Do Need help?  type *help `)
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
 
-handler.customPrefix = /^(Xcel|Excel)$/i
+handler.customPrefix = /^(Star|Excel)$/i
 handler.command = new RegExp
 
 export default handler
