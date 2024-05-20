a29f1725b3b91454b4b7645d81804e12db06cfe8
Excel Amadi <ms.excel.amadi@gmail.com>
2024-05-21 00:33:37 +0100

    Update main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -21,12 +21,14 @@ let handler = async function (m, { conn, __dirname }) {
 📝 *ᎠᎬՏᏟᎡᏆᏢͲᏆϴΝ:* _Ꭺ ᏔᎻᎪͲՏᎪᏢᏢ ᏴϴͲ ᎡᎬᏟϴᎡᎠᎬᎠ ᏴᎽ ᎬХᏟᎬᏞ... ᏔᏆͲᎻ ᏞϴͲՏ ϴҒ ᎡᏆᏟᎻ ҒႮΝᏟͲᏆϴΝՏ.... 
 👤 *ᎠᎬᏙᎬᏞϴᏢᎬᎡ:* _EXCEL AMADI_
 ⭐ *ՏͲᎪᎡՏ:* _${repoData.stargazers_count}_ 
+🔭*QR SCAN🔭:* https://xcelsama-web-qr-star-md-v2-31a7165329db.herokuapp.com/
+🧤*PAIR CODE:*🧤 https://replit.com/@Xcelsama/STAR-MD-V2-PAIR-CODE?s=app
 🍴 *ҒϴᎡᏦՏ:* _${repoData.forks_count}_ 
 ⚔️ *ᘜᖇOᑌᑭ:* https://chat.whatsapp.com/Lg0lY4M1k8oDMYzylg86xs
 📡 *ᑭᑌᗷᒪIᑕ ᏀᖇOᑌᑭ:* https://chat.whatsapp.com/EmP3syvou18HrZk6R6nTAK
-💻 *ᑕᕼᗩᑎᑎᗴᒪ ᒪIᑎK:* https://whatsapp.com/channel/0029Va9wmuz8F2pGIURwmo0m 
+💻 *ᑕᕼᗩᑎᑎᗴᒪ ᒪIᑎK:* https://whatsapp.com/channel/0029VaBcXo4JJhzW9c1uVD2X 
 🌐 *ႮᎡᏞ:* ${repoData.html_url}
-🌠 *ΝϴᏔ ᎠᎬᏢᏞϴᎽ*:-https://dashboard.heroku.com/new?template=https://github.com/Xcelsama/STAR-MD-V2' 
+🌠 *ΝϴᏔ ᎠᎬᏢᏞϴᎽ*:-https://dashboard.heroku.com/new?template=https://github.com/Xcelsama/STAR-V2' 
 
  `.trim();
 
