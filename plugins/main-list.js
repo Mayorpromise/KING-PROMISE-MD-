d8c596de1f3b7ecfa029a9f952ff6e7d8adeb320
Excel Amadi <ms.excel.amadi@gmail.com>
2024-05-20 22:53:16 +0100

    Update main-list.js

--- plugins/main-list.js
+++ plugins/main-list.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, usedPrefix, command}) => {
     
       let lkr;
       switch (command) {
-        case 'list':
+        case 'list2':
 lkr = "*Get excited, here are your options:*\n\n" +
 "🤖 *" + usedPrefix + "botmenu* - The Bot's secret control panel. What's your command, oh great one?\n\n" +
 "👑 *" + usedPrefix + "ownermenu* - The sacred scroll only for the chosen one. Yep, that's you, Boss!\n\n" +    
@@ -358,7 +358,7 @@ break;
           lkr = `use command ${usedPrefix}nsfw`; // 
           break;
           case 'logomenu':
-          lkr = `use ${usedPrefix}logo to see all options \ngfx cmd upto 12`; // 
+          lkr = `use ${usedPrefix}gfx to see all options \ngfx cmd upto 12`; // 
           break;
         default:
           lkr = `Invalid command. Type ${usedPrefix}list to see available options.`;
@@ -372,7 +372,7 @@ break;
     
     handler.help = ['list', 'botmenu', 'ownermenu', 'groupmenu', 'dlmenu', 'downloadermenu', 'economymenu', 'funmenu', 'gamemenu', 'stickermenu', 'nsfwmenu', 'logomenu', 'toolmenu','animemenu2','animemenu'];
     handler.tags = ['main'];
-    handler.command = ['list', 'botmenu', 'ownermenu', 'groupmenu', 'dlmenu', 'downloadermenu', 'economymenu', 'funmenu', 'gamemenu', 'stickermenu', 'nsfwmenu', 'logomenu', 'toolmenu','animemenu2','animemenu'];
+    handler.command = ['list2', 'botmenu', 'ownermenu', 'groupmenu', 'dlmenu', 'downloadermenu', 'economymenu', 'funmenu', 'gamemenu', 'stickermenu', 'nsfwmenu', 'logomenu', 'toolmenu','animemenu2','animemenu'];
     
     export default handler
     
