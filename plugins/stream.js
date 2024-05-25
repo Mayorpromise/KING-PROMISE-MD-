225c52915fb90e3d0fac075c9e14eaedb7b3796c
Excel Amadi <ms.excel.amadi@gmail.com>
2024-05-25 19:23:38 +0100

    Update plugins

---
+++ plugins/stream.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+let handler = async (m, { text, conn, usedPrefix, command }) => {
+
+
+ await conn.sendButton(m.chat,result, author, 'https://telegra.ph/file/ff9545dd2cce2488dd30d.jpg', [['Help', `${usedPrefix}channel`]], null, [['STREAM ANIME',https://www.aniplus-asia.com``]], m)
+
+  }
+
+handler.help = ['stream']
+
+handler.tags = ['New']
+
+handler.command = ['stream']
+
+export default handler
