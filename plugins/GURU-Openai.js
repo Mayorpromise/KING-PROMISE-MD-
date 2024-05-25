cce45646c7dd3c7e9858a9ba121a19a5fe9b2762
Excel Amadi <ms.excel.amadi@gmail.com>
2024-05-25 16:45:26 +0100

    Update GURU-Openai.js

--- plugins/GURU-Openai.js
+++ plugins/GURU-Openai.js
@@ -26,21 +26,21 @@ let handler = async (m, { text, conn, usedPrefix, command }) => {
         throw new Error('No valid JSON response from the first API')
       }
 
-      await conn.sendButton(m.chat,result, author, 'https://telegra.ph/file/c3f9e4124de1f31c1c6ae.jpg', [['Script', `.sc`]], null, [['Follow Me', `https://github.com/Guru322`]], m)
+      await conn.sendButton(m.chat,result, author, 'https://telegra.ph/file/f6908fee9f896b7cb6e63.jpg', [['Script', `${usedPrefix}script`]], null, [['Fork Repo', `https://github.com/Xcelsama/STAR-MD-V2/fork`]], m)
       m.react(done)
     } catch (error) {
       console.error('Error from the first API:', error)
 
       //const model = 'llama'
      // const senderNumber = m.sender.replace(/[^0-9]/g, '')
-      //const session = `GURU_BOT_${senderNumber}`
+      //const session = `xlicon_bot_${senderNumber}`
       const guru2 = `https://ultimetron.guruapi.tech/gpt3?prompt=${prompt}`
 
       let response = await fetch(guru2)
       let data = await response.json()
       let result = data.completion
 
-      await conn.sendButton(m.chat,result, author, 'https://telegra.ph/file/c3f9e4124de1f31c1c6ae.jpg', [['Script', `.sc`]], null, [['Follow Me', `https://github.com/Guru322`]], m)
+     await conn.sendButton(m.chat,result, author, 'https://telegra.ph/file/f6908fee9f896b7cb6e63.jpg', [['Script', `.sc`]], null, [['Fork Repo', `https://github.com/Xcelsama/STAR-MD-V2/fork`]], m)
       m.react(done)
     }
   } catch (error) {
@@ -50,6 +50,6 @@ let handler = async (m, { text, conn, usedPrefix, command }) => {
 }
 handler.help = ['chatgpt']
 handler.tags = ['AI']
-handler.command = ['hi', 'chatgpt', 'ai', 'gpt']
+handler.command = ['bro', 'chatgpt', 'ai', 'gpt']
 
 export default handler
