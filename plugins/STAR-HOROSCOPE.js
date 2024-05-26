a451707e8eeddb248043a2ad51d6310448517c75
Excel Amadi <ms.excel.amadi@gmail.com>
2024-05-26 20:49:28 +0100

    Update plugins

---
+++ plugins/STAR-HOROSCOPE.js
@@ -0,0 +1,26 @@
+import fetch from 'node-fetch';
+
+var horoscopeHandler = async (m, { conn, args }) => {
+  let sign = args[0];
+  if (!sign) return conn.sendMessage(m.chat, "Please provide a zodiac sign.");
+
+  let url = `https://aztro.sameerkumar.website/?sign=${sign}&day=today`;
+
+  let response = await fetch(url, { method: 'POST' });
+  let data = await response.json();
+
+  let horoscopeInfo = `
+*Horoscope for ${sign}:*
+- Date: ${data.current_date}
+- Compatibility: ${data.compatibility}
+- Mood: ${data.mood}
+- Color: ${data.color}
+- Lucky Number: ${data.lucky_number}
+- Description: ${data.description}
+`;
+
+  await conn.sendMessage(m.chat, horoscopeInfo);
+};
+
+handler.command = /^(horoscope|zodiac)$/i;
+export default horoscopeHandler;
