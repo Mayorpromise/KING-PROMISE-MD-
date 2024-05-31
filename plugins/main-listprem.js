ec11cff0e3a2e944862a357b12c90adbac53c6e8
Nwankwo Promise <154396061+Mayorpromise@users.noreply.github.com>
2024-05-31 13:05:20 +0100

    Update main-listprem.js

--- plugins/main-listprem.js
+++ plugins/main-listprem.js
@@ -1,7 +1,7 @@
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 let prem = global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid) 
-let teks = `▢ *PREMIUM USERS OF STAR
+let teks = `▢ *PREMIUM USERS OF KING-PROMISE
 *\n─────────────\n` + prem.map(v => '- @' + v.replace(/@.+/, '')).join`\n`
 m.reply(teks, null, {mentions: conn.parseMention(teks)})
 
