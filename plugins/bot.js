9e1d58c3c9492dcb8f55db182adf0f4b3842347f
Nwankwo Promise <154396061+Mayorpromise@users.noreply.github.com>
2024-05-31 20:34:22 +0100

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -5,7 +5,7 @@ let name = conn.getName(m.sender)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
 let av = `./Assets/mp3/${pickRandom(["STAR", "STAR1", "STAR2", "STAR3", "STAR4"])}.mp3`
 
-m.reply( `*Hello ${taguser} Am STAR-MD-V2 created by Excel Do Need help?  type *help *`)
+m.reply( `*Hello ${taguser} Am KING-PROMISE MD created by Mayor. Do Need help?  type .help*`)
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
 
