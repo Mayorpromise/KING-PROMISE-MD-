47285d6b458490472bb9b9cf8dc181ded6c5289f
Nwankwo Promise <154396061+Mayorpromise@users.noreply.github.com>
2024-06-01 08:15:29 +0100

    Update channel.js

--- plugins/channel.js
+++ plugins/channel.js
@@ -1,11 +1,11 @@
 let handler = async m => m.reply(`
 
-╭⭑⭑⭑★✪•••*ՏͲᎪᎡ*•••✪★⭑⭑⭑
-│ 📂 *BOT Name:* _ՏͲᎪᎡ-ᎷᎠ-Ꮩ2_
-│ 📝 *Description:* _I'm STAR-MD-V2. A  
-┃MultiDevice WhatsApp bot with rich features  ┃Created BY +2347045035241._
-│ 👤 *Owner:* _乇乂匚乇ㄥ_
-│ 🌐 *Channel:* https://whatsapp.com/channel/0029VaBcXo4JJhzW9c1uVD2X
+╭⭑⭑⭑★✪•••*KING-PROMISE*•••✪★⭑⭑⭑
+│ 📂 *BOT Name:* _KING-PROMISE MD_
+│ 📝 *Description:* _I'm KING-PROMISE MD. A  
+┃MultiDevice WhatsApp bot with rich features  ┃Created BY +2349164717432._
+│ 👤 *Owner:* _𝙼̷𝙰̷𝚈̷𝙾̷𝚁̷_
+│ 🌐 *Channel:* https://whatsapp.com/channel/0029VabKDye3bbV4Fu13US2N
 ╰━━━━━━━━━━━━━━━━╯
 `.trim())
 handler.help = ['channel']
